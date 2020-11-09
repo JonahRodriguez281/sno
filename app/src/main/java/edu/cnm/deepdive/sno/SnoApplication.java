13c91026417368582416dfba62d9055b87c6b2f2
anayadrian1 <anayadrian1@gmail.com>
2020-11-08 22:51:42 -0700

    Google sign-in can't sign in

--- app/src/main/java/edu/cnm/deepdive/sno/SnoApplication.java
+++ app/src/main/java/edu/cnm/deepdive/sno/SnoApplication.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.sno;
 
 import android.app.Application;
 import com.facebook.stetho.Stetho;
+import edu.cnm.deepdive.sno.service.GoogleSignInService;
 import edu.cnm.deepdive.sno.service.SnoDatabase;
 import io.reactivex.schedulers.Schedulers;
 
@@ -11,6 +12,7 @@ public class SnoApplication extends Application {
   public void onCreate() {
     super.onCreate();
     Stetho.initializeWithDefaults(this);
+    GoogleSignInService.setContext(this);
     SnoDatabase.setContext(this);
     SnoDatabase.getInstance().getMountainDao().delete()
         .subscribeOn(Schedulers.io())
